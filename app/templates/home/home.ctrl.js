2973d5c3eedc849dc87d0b16c597ca9c2509b256
marielelyukh <maria@dorsetcreative.tech>
2018-10-29 02:12:39 +0200

    slider 60%

--- app/templates/home/home.ctrl.js
+++ app/templates/home/home.ctrl.js
@@ -10,9 +10,25 @@
         $('.slider').slick({
             slidesToShow: 1,
             slidesToScroll: 1,
-            dots: true,
-            infinite: true,
-            cssEase: 'linear'
+            autoplay: true,
+            autoplaySpeed: 2000,
+            infinite : false
         });
+        /*REMOVE or comment below code to see behavior of infinite false setting below code handles removing of arrows*/
+        $('#slider').on('afterChange', function(){
+            console.log($('#slider').slick('slickCurrentSlide'));
+            var currentSlide = $('#slider').slick('slickCurrentSlide');
+            $('.slick-prev').hide();
+            $('.slick-next').hide();
+
+        });
+
+        $(document).ready(function(){
+            var currentSlide = $('#slider').slick('slickCurrentSlide');
+            $('.slick-prev').hide();
+            $('.slick-next').hide();
+
+        });
+        /*REMOVE TILL HERE*/
     }
 })();
